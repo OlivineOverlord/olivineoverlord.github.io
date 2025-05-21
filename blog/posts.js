350c81c704bfbf0780403816dbd8524159e1d449
Josh Shea <joshuajshea@gmail.com>
2025-05-21 21:57:38 +0100

    blog update

--- blog/posts.js
+++ blog/posts.js
@@ -4,7 +4,7 @@ const posts = [
     title: "Athletic Thinking in Academic Life",
     date: "2024-12-20",
     description: "How Olympic discipline shaped my research mindset.",
-    href: "blog-athlete-mindset.html"
+    href: "blog/blog-athlete-mindset.html"
   },
   // Add more blog posts below
 ];
