f16e3c3529678904ba0efc09fea52eca43f8af3d
Josh Shea <joshuajshea@gmail.com>
2025-06-26 17:14:15 +0100

    blog and cv update

--- blog/posts.js
+++ blog/posts.js
@@ -1,10 +1,10 @@
 // blog/posts.js
 const posts = [
   {
-  title: "Applied Science Isn’t What You Think It Is",
+  title: "The Problem with Applied Science",
   date: "2025-06-26",
   href: "blog/blog-applied-science.html",
-  description: "The push for 'applied science' and a misunderstanding of science.",
+  description: "The push for 'applied science' and a misunderstanding of science itself.",
   },
   // Add more blog posts below
 ];
